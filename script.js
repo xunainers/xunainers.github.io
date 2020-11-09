ed457e23a592f939c101ae23ab920dbbb4da595e
xunainers <70502426+xunainers@users.noreply.github.com>
2020-11-09 01:43:54 -0300

    Clickable events

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var Abas = document.querySelectorAll(".opcao");
 Abas[0].focus();
 var vData = new Date();
@@ -12,3 +13,24 @@ else
   hoje = vData.getDate() + '/' + vData.getYear();  
 
 document.getElementById("info").innerHTML += '<b>Data: '+ hoje +'</b>';
+
+
+$('.produto1').on('click', function(e) {
+   alert( 'Vai pedir IPA!' );
+});
+
+$('.produto2').on('click', function(e) {
+  alert( 'Vai pedir Pilsen!' );
+});
+
+$('.produto3').on('click', function(e) {
+  alert( 'Vai pedir Dry Stout!' );
+});
+
+$('.produto4').on('click', function(e) {
+  alert( 'Vai pedir Lagger!' );
+});
+
+$('.produto5').on('click', function(e) {
+  alert( 'Vai pedir Gin Tonika!' );
+});
